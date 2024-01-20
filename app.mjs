80cb440f0cf303f49b825ceba25238e398cf9169
Somnath Mishra <120735604+Somnath-Mishra@users.noreply.github.com>
2024-01-20 17:51:20 +0530

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,17 +1,14 @@
 import express from 'express';
-// import http from 'http';
 import path from 'path';
-// import { Server as SocketServer } from 'socket.io';
-// import { v4 as uuid4 } from 'uuid';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import {listEvents,authorize,addEventDetails} from './calender/listEvent.mjs'
 
-
 const app = express();
-// const server = http.createServer(app);
-// const io = new SocketServer(server);
 
 const PORT =3001;
+
+mongoose.connect("mongodb://127.0.0.1:27017/DoctorsDB")
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
@@ -19,14 +16,13 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// let events=[];
 
-// app.get('/', (req, res) => {
-//   res.redirect(`/${uuid4()}`);
-// });
-
-app.get('/', (req, res) => {
-    res.render("index.ejs");
+app.get('/', async(req, res) => {
+    let mode='online';
+    const client=await authorize();
+    const events=await listEvents(client);
+    console.log(events);
+    res.render("index.ejs",{events:events,mode:mode});
 })
 
 app.get('/problem', (req, res) => {
@@ -57,22 +53,11 @@ app.get('/bookAppointment',(req,res)=>{
 app.post('/bookAppointment',async (req,res)=>{
 
   let summary=req.body.summary;
-  let location=req.body.location;
   let description=req.body.description;
-  let startDateTime=req.body.description;
-  let endDateTime=req.body.endDateTime;
-  let timezone=req.body.timeZone;
-  let attendees=req.body.attendees;
-  const client=await authorize();
-  addEventDetails(summary,location,description,startDateTime,endDateTime,timezone,attendees,client)
+  let startDateTime=req.body.startDateTime;
+  addEventDetails(summary,description,startDateTime)
   res.redirect('/appointment');
 })
-
-// app.get('/:room', (req, res) => {
-//   res.render('room', { roomId: req.params.room });
-// });
-
-
 
 app.listen(PORT,()=>{
     console.log(`Server is started at PORT ${PORT}`);
